9854562fa531bc1aaedd03d3e9e6bd02bdb37bb7
Blaz77 <lugusor@gmail.com>
2017-11-26 21:00:26 -0300

    Software para PC

---
+++ SoftwarePC/InterfazLavadora/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace InterfazLavadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrCheckComPorts = new System.Windows.Forms.Timer(this.components);
+            this.cmbPortName = new System.Windows.Forms.ComboBox();
+            this.lblComPort = new System.Windows.Forms.Label();
+            this.cmbBaudRate = new System.Windows.Forms.ComboBox();
+            this.lblBaudRate = new System.Windows.Forms.Label();
+            this.btnOpenPort = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lblSend = new System.Windows.Forms.Label();
+            this.txtSendData = new System.Windows.Forms.TextBox();
+            this.rtfTerminal = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // tmrCheckComPorts
+            // 
+            this.tmrCheckComPorts.Enabled = true;
+            this.tmrCheckComPorts.Interval = 500;
+            // 
+            // cmbPortName
+            // 
+            this.cmbPortName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPortName.FormattingEnabled = true;
+            this.cmbPortName.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6"});
+            this.cmbPortName.Location = new System.Drawing.Point(20, 36);
+            this.cmbPortName.Name = "cmbPortName";
+            this.cmbPortName.Size = new System.Drawing.Size(67, 21);
+            this.cmbPortName.TabIndex = 3;
+            // 
+            // lblComPort
+            // 
+            this.lblComPort.AutoSize = true;
+            this.lblComPort.Location = new System.Drawing.Point(17, 20);
+            this.lblComPort.Name = "lblComPort";
+            this.lblComPort.Size = new System.Drawing.Size(56, 13);
+            this.lblComPort.TabIndex = 2;
+            this.lblComPort.Text = "COM Port:";
+            // 
+            // cmbBaudRate
+            // 
+            this.cmbBaudRate.FormattingEnabled = true;
+            this.cmbBaudRate.Items.AddRange(new object[] {
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.cmbBaudRate.Location = new System.Drawing.Point(108, 36);
+            this.cmbBaudRate.Name = "cmbBaudRate";
+            this.cmbBaudRate.Size = new System.Drawing.Size(69, 21);
+            this.cmbBaudRate.TabIndex = 5;
+            // 
+            // lblBaudRate
+            // 
+            this.lblBaudRate.AutoSize = true;
+            this.lblBaudRate.Location = new System.Drawing.Point(105, 20);
+            this.lblBaudRate.Name = "lblBaudRate";
+            this.lblBaudRate.Size = new System.Drawing.Size(61, 13);
+            this.lblBaudRate.TabIndex = 4;
+            this.lblBaudRate.Text = "Baud Rate:";
+            // 
+            // btnOpenPort
+            // 
+            this.btnOpenPort.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpenPort.Location = new System.Drawing.Point(330, 34);
+            this.btnOpenPort.Name = "btnOpenPort";
+            this.btnOpenPort.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenPort.TabIndex = 7;
+            this.btnOpenPort.Text = "&Open Port";
+            this.btnOpenPort.Click += new System.EventHandler(this.btnOpenPort_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.Location = new System.Drawing.Point(330, 94);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 10;
+            this.btnSend.Text = "Send";
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lblSend
+            // 
+            this.lblSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblSend.AutoSize = true;
+            this.lblSend.Location = new System.Drawing.Point(17, 99);
+            this.lblSend.Name = "lblSend";
+            this.lblSend.Size = new System.Drawing.Size(61, 13);
+            this.lblSend.TabIndex = 8;
+            this.lblSend.Text = "Send &Data:";
+            // 
+            // txtSendData
+            // 
+            this.txtSendData.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSendData.Location = new System.Drawing.Point(86, 96);
+            this.txtSendData.Name = "txtSendData";
+            this.txtSendData.Size = new System.Drawing.Size(238, 20);
+            this.txtSendData.TabIndex = 9;
+            // 
+            // rtfTerminal
+            // 
+            this.rtfTerminal.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtfTerminal.Location = new System.Drawing.Point(12, 145);
+            this.rtfTerminal.Name = "rtfTerminal";
+            this.rtfTerminal.Size = new System.Drawing.Size(393, 135);
+            this.rtfTerminal.TabIndex = 11;
+            this.rtfTerminal.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 373);
+            this.Controls.Add(this.rtfTerminal);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.lblSend);
+            this.Controls.Add(this.txtSendData);
+            this.Controls.Add(this.btnOpenPort);
+            this.Controls.Add(this.cmbBaudRate);
+            this.Controls.Add(this.lblBaudRate);
+            this.Controls.Add(this.cmbPortName);
+            this.Controls.Add(this.lblComPort);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrCheckComPorts;
+        private System.Windows.Forms.ComboBox cmbPortName;
+        private System.Windows.Forms.Label lblComPort;
+        private System.Windows.Forms.ComboBox cmbBaudRate;
+        private System.Windows.Forms.Label lblBaudRate;
+        private System.Windows.Forms.Button btnOpenPort;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label lblSend;
+        private System.Windows.Forms.TextBox txtSendData;
+        private System.Windows.Forms.RichTextBox rtfTerminal;
+    }
+}
+
---
+++ SoftwarePC/InterfazLavadora/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace InterfazLavadora
+{
+    public enum DataMode { Text, Hex }
+
+    public enum LogMsgType { Incoming, Outgoing, Normal, Warning, Error };
+
+    public partial class Form1 : Form
+    {
+        private SerialPort comport = new SerialPort();
+        private Color[] LogMsgTypeColor = { Color.Blue, Color.Green, Color.Black, Color.Orange, Color.Red };
+
+
+        private DataMode CurrentDataMode
+        {
+            get
+            {
+                return DataMode.Text;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeControlValues();
+
+            EnableControls();
+
+
+            comport.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+        }
+
+        private void InitializeControlValues()
+        {
+
+            RefreshComPortList();
+            
+            if (cmbPortName.Items.Count > 0) cmbPortName.SelectedIndex = cmbPortName.Items.Count - 1;
+            else
+            {
+                MessageBox.Show(this, "There are no COM Ports detected on this computer.\nPlease install a COM Port and restart this app.", "No COM Ports Installed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+            }
+        }
+
+        private void EnableControls()
+        {
+            txtSendData.Enabled = btnSend.Enabled = comport.IsOpen;
+
+            if (comport.IsOpen) btnOpenPort.Text = "&Close Port";
+            else btnOpenPort.Text = "&Open Port";
+        }
+
+        private void btnOpenPort_Click(object sender, EventArgs e)
+        {
+            bool error = false;
+
+            if (comport.IsOpen) comport.Close();
+            else
+            {
+                comport.BaudRate = int.Parse(cmbBaudRate.Text);
+                comport.DataBits = 8;
+                comport.StopBits = StopBits.One;
+                comport.Parity = Parity.None;
+                comport.PortName = cmbPortName.Text;
+
+                try
+                {
+                    comport.Open();
+                }
+                catch (UnauthorizedAccessException) { error = true; }
+                catch (IOException) { error = true; }
+                catch (ArgumentException) { error = true; }
+
+                if (error) MessageBox.Show(this, "Could not open the COM port.  Most likely it is already in use, has been removed, or is unavailable.", "COM Port Unavalible", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+            }
+
+            EnableControls();
+
+            if (comport.IsOpen)
+            {
+                txtSendData.Focus();
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        { SendData(); }
+
+        private void SendData()
+        {
+            if (CurrentDataMode == DataMode.Text)
+            {
+                comport.Write(txtSendData.Text);
+
+                Log(LogMsgType.Outgoing, txtSendData.Text + "\n");
+            }
+            else
+            {
+                try
+                {
+                    byte[] data = HexStringToByteArray(txtSendData.Text);
+
+                    comport.Write(data, 0, data.Length);
+
+                    Log(LogMsgType.Outgoing, ByteArrayToHexString(data) + "\n");
+                }
+                catch (FormatException)
+                {
+                    Log(LogMsgType.Error, "Not properly formatted hex string: " + txtSendData.Text + "\n");
+                }
+            }
+            txtSendData.SelectAll();
+        }
+
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (!comport.IsOpen) return;
+
+
+            if (CurrentDataMode == DataMode.Text)
+            {
+                string data = comport.ReadExisting();
+
+                Log(LogMsgType.Incoming, data);
+            }
+            else
+            {
+                int bytes = comport.BytesToRead;
+
+                byte[] buffer = new byte[bytes];
+
+                comport.Read(buffer, 0, bytes);
+
+                Log(LogMsgType.Incoming, ByteArrayToHexString(buffer));
+            }
+        }
+
+        private void tmrCheckComPorts_Tick(object sender, EventArgs e)
+        {
+            RefreshComPortList();
+        }
+
+        private void Log(LogMsgType msgtype, string msg)
+        {
+            rtfTerminal.Invoke(new EventHandler(delegate
+            {
+                rtfTerminal.SelectedText = string.Empty;
+                rtfTerminal.SelectionFont = new Font(rtfTerminal.SelectionFont, FontStyle.Bold);
+                rtfTerminal.SelectionColor = LogMsgTypeColor[(int)msgtype];
+                rtfTerminal.AppendText(msg);
+                rtfTerminal.ScrollToCaret();
+            }));
+        }
+
+        private byte[] HexStringToByteArray(string s)
+        {
+            s = s.Replace(" ", "");
+            byte[] buffer = new byte[s.Length / 2];
+            for (int i = 0; i < s.Length; i += 2)
+                buffer[i / 2] = (byte)Convert.ToByte(s.Substring(i, 2), 16);
+            return buffer;
+        }
+        
+        private string ByteArrayToHexString(byte[] data)
+        {
+            StringBuilder sb = new StringBuilder(data.Length * 3);
+            foreach (byte b in data)
+                sb.Append(Convert.ToString(b, 16).PadLeft(2, '0').PadRight(3, ' '));
+            return sb.ToString().ToUpper();
+        }
+
+        private void RefreshComPortList()
+        {
+            string selected = RefreshComPortList(cmbPortName.Items.Cast<string>(), cmbPortName.SelectedItem as string, comport.IsOpen);
+
+            if (!String.IsNullOrEmpty(selected))
+            {
+                cmbPortName.Items.Clear();
+                cmbPortName.Items.AddRange(OrderedPortNames());
+                cmbPortName.SelectedItem = selected;
+            }
+        }
+
+        private string RefreshComPortList(IEnumerable<string> PreviousPortNames, string CurrentSelection, bool PortOpen)
+        {
+            string selected = null;
+
+            string[] ports = SerialPort.GetPortNames();
+
+            bool updated = PreviousPortNames.Except(ports).Count() > 0 || ports.Except(PreviousPortNames).Count() > 0;
+
+            if (updated)
+            {
+                ports = OrderedPortNames();
+
+                string newest = SerialPort.GetPortNames().Except(PreviousPortNames).OrderBy(a => a).LastOrDefault();
+
+                if (PortOpen)
+                {
+                    if (ports.Contains(CurrentSelection)) selected = CurrentSelection;
+                    else if (!String.IsNullOrEmpty(newest)) selected = newest;
+                    else selected = ports.LastOrDefault();
+                }
+                else
+                {
+                    if (!String.IsNullOrEmpty(newest)) selected = newest;
+                    else if (ports.Contains(CurrentSelection)) selected = CurrentSelection;
+                    else selected = ports.LastOrDefault();
+                }
+            }
+
+            return selected;
+        }
+
+        private string[] OrderedPortNames()
+        {
+            int num;
+
+            return SerialPort.GetPortNames().OrderBy(a => a.Length > 3 && int.TryParse(a.Substring(3), out num) ? num : 0).ToArray();
+        }
+    }
+}
